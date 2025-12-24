7027bd72b0b0cee91f31b319c5881ba32b21744d
hamoohamooha2-crypto <hamoohamooha2@gmail.com>
2025-12-24 11:50:19 +0700

    Update ModEntry.cs

--- ModEntry.cs
+++ ModEntry.cs
@@ -22,17 +22,13 @@ namespace CustomBackpack
         public static IModHelper SHelper;
         public static ModConfig Config;
         public static ModEntry context;
-
         public static string dictPath = "platinummyr.CustomBackpackFramework/dictionary";
-
         public static Dictionary<int, BackPackData> dataDict = new Dictionary<int, BackPackData>();
         public static int IDOffset = 0;
         public static Texture2D scrollTexture;
         public static Texture2D handleTexture;
-
         public static PerScreen<IClickableMenu> lastMenu = new PerScreen<IClickableMenu>();
         public static PerScreen<int> pressTime = new PerScreen<int>();
-
         public static PerScreen<int> oldScrollValue = new PerScreen<int>();
         public static PerScreen<int> oldCapacity = new PerScreen<int>();
         public static PerScreen<int> oldRows = new PerScreen<int>();
@@ -46,12 +42,9 @@ namespace CustomBackpack
         public override void Entry(IModHelper helper)
         {
             Config = Helper.ReadConfig<ModConfig>();
-
-            if (!Config.ModEnabled)
-                return;
+            if (!Config.ModEnabled) return;
 
             context = this;
-
             SMonitor = Monitor;
             SHelper = helper;
 
@@ -62,7 +55,7 @@ namespace CustomBackpack
             helper.Events.Input.ButtonPressed += Input_ButtonPressed;
             helper.Events.GameLoop.UpdateTicked += GameLoop_UpdateTicked;
 
-            helper.ConsoleCommands.Add("custombackpack", "Manually set backpack slots. Usage: custombackpack <slotnumber>", SetSlots);
+            helper.ConsoleCommands.Add("custombackpack", "Usage: custombackpack <slotnumber>", SetSlots);
 
             var harmony = new Harmony(ModManifest.UniqueID);
 
@@ -74,16 +67,6 @@ namespace CustomBackpack
             harmony.Patch(
                 original: AccessTools.Method(typeof(InventoryMenu), nameof(InventoryMenu.hover), new Type[] { typeof(int), typeof(int) }),
                 prefix: new HarmonyMethod(typeof(ObjectPatches), nameof(ObjectPatches.InventoryMenu_hover_Prefix))
-            );
-
-            harmony.Patch(
-                original: AccessTools.Method(typeof(ShopMenu), nameof(ShopMenu.performHoverAction), new Type[] { typeof(int), typeof(int) }),
-                prefix: new HarmonyMethod(typeof(ObjectPatches), nameof(ObjectPatches.ShopMenu_performHoverAction_Prefix))
-            );
-
-            harmony.Patch(
-                original: AccessTools.Method(typeof(ShopMenu), nameof(ShopMenu.receiveScrollWheelAction), new Type[] { typeof(int) }),
-                prefix: new HarmonyMethod(typeof(ObjectPatches), nameof(ObjectPatches.ShopMenu_receiveScrollWheelAction_Prefix))
             );
 
             harmony.Patch(
@@ -102,29 +85,9 @@ namespace CustomBackpack
             );
 
             harmony.Patch(
-                original: AccessTools.Method(typeof(InventoryMenu), nameof(InventoryMenu.setUpForGamePadMode)),
-                postfix: new HarmonyMethod(typeof(ObjectPatches), nameof(ObjectPatches.InventoryMenu_setUpForGamePadMode_Postfix))
-            );
-
-            harmony.Patch(
-                original: AccessTools.Method(typeof(IClickableMenu), nameof(IClickableMenu.applyMovementKey), new Type[] { typeof(int) }),
-                prefix: new HarmonyMethod(typeof(ObjectPatches), nameof(ObjectPatches.IClickableMenu_applyMovementKey_Prefix))
-            );
-
-            harmony.Patch(
-                original: AccessTools.Method(typeof(ItemGrabMenu), "customSnapBehavior", new Type[] { typeof(int), typeof(int), typeof(int) }),
-                prefix: new HarmonyMethod(typeof(ObjectPatches), nameof(ObjectPatches.ItemGrabMenu_customSnapBehavior_Prefix))
-            );
-
-            harmony.Patch(
                 original: AccessTools.Method(typeof(InventoryMenu), nameof(InventoryMenu.draw), new Type[] { typeof(SpriteBatch), typeof(int), typeof(int), typeof(int) }),
                 prefix: new HarmonyMethod(typeof(ObjectPatches), nameof(ObjectPatches.InventoryMenu_draw_Prefix)),
                 postfix: new HarmonyMethod(typeof(ObjectPatches), nameof(ObjectPatches.InventoryMenu_draw_Postfix))
-            );
-
-            harmony.Patch(
-                original: AccessTools.Method(typeof(SeedShop), nameof(SeedShop.draw), new Type[] { typeof(SpriteBatch) }),
-                prefix: new HarmonyMethod(typeof(ObjectPatches), nameof(ObjectPatches.SeedShop_draw_Prefix))
             );
 
             harmony.Patch(
@@ -137,20 +100,8 @@ namespace CustomBackpack
                 prefix: new HarmonyMethod(typeof(ObjectPatches), nameof(ObjectPatches.GameLocation_answerDialogueAction_Prefix))
             );
 
-            harmony.Patch(
-                original: AccessTools.PropertyGetter(typeof(SpecialItem), "displayName"),
-                prefix: new HarmonyMethod(typeof(ObjectPatches), nameof(ObjectPatches.SpecialItem_displayName_Prefix))
-            );
-
-            harmony.Patch(
-                original: AccessTools.Method(typeof(SpecialItem), nameof(SpecialItem.getTemporarySpriteForHoldingUp), new Type[] { typeof(Vector2) }),
-                prefix: new HarmonyMethod(typeof(ObjectPatches), nameof(ObjectPatches.SpecialItem_getTemporarySpriteForHoldingUp_Prefix))
-            );
-
-            harmony.Patch(
-                original: AccessTools.Method(typeof(Farmer), nameof(Farmer.shiftToolbar), new Type[] { typeof(bool) }),
-                prefix: new HarmonyMethod(typeof(ObjectPatches), nameof(ObjectPatches.Farmer_shiftToolbar_Prefix))
-            );
+            harmony.Patch(original: AccessTools.Method(typeof(InventoryMenu), nameof(InventoryMenu.setUpForGamePadMode)), postfix: new HarmonyMethod(typeof(ObjectPatches), nameof(ObjectPatches.InventoryMenu_setUpForGamePadMode_Postfix)));
+            harmony.Patch(original: AccessTools.Method(typeof(Farmer), nameof(Farmer.shiftToolbar), new Type[] { typeof(bool) }), prefix: new HarmonyMethod(typeof(ObjectPatches), nameof(ObjectPatches.Farmer_shiftToolbar_Prefix)));
 
             scrollTexture = new Texture2D(Game1.graphics.GraphicsDevice, 1, 1);
             scrollTexture.SetData(new Color[] { Config.BackgroundColor });
@@ -161,138 +112,78 @@ namespace CustomBackpack
         private void GameLoop_UpdateTicked(object sender, StardewModdingAPI.Events.UpdateTickedEventArgs e)
         {
             if (scrolling.Value && (Game1.activeClickableMenu is null || Game1.input.GetMouseState().LeftButton != ButtonState.Pressed))
-            {
                 scrolling.Value = false;
-            }
+            
             var newScrollValue = Game1.input.GetMouseState().ScrollWheelValue;
-            if (oldScrollValue.Value > newScrollValue)
-                scrollChange.Value = 1;
-            else if (oldScrollValue.Value < newScrollValue)
-                scrollChange.Value = -1;
-            else
-                scrollChange.Value = 0;
+            scrollChange.Value = (oldScrollValue.Value > newScrollValue) ? 1 : (oldScrollValue.Value < newScrollValue ? -1 : 0);
             oldScrollValue.Value = newScrollValue;
         }
 
         private void Input_ButtonPressed(object sender, StardewModdingAPI.Events.ButtonPressedEventArgs e)
         {
             if (Game1.activeClickableMenu is not null && e.Button == SButton.MouseLeft && scrollArea.Value.Contains(Game1.getMouseX(), Game1.getMouseY()))
-            {
                 scrolling.Value = true;
-            }
         }
 
-        public override object GetApi()
-        {
-            return new CustomBackpackApi();
-        }
+        public override object GetApi() => new CustomBackpackApi();
 
         private void SetSlots(string arg1, string[] arg2)
         {
             if (arg2.Length == 1 && int.TryParse(arg2[0], out int slots))
-            {
                 SetPlayerSlots(slots);
-            }
         }
 
         public void SetPlayerSlots(int slots)
         {
+            if (Game1.player == null) return;
             Game1.player.MaxItems = slots;
             while (Game1.player.Items.Count < Game1.player.MaxItems)
-            {
                 Game1.player.Items.Add(null);
-            }
         }
 
-        private void GameLoop_DayStarted(object sender, StardewModdingAPI.Events.DayStartedEventArgs e)
-        {
-            LoadDict();
-        }
-
-        private void GameLoop_SaveLoaded(object sender, StardewModdingAPI.Events.SaveLoadedEventArgs e)
-        {
-            LoadDict();
-        }
+        private void GameLoop_DayStarted(object sender, StardewModdingAPI.Events.DayStartedEventArgs e) => LoadDict();
+        private void GameLoop_SaveLoaded(object sender, StardewModdingAPI.Events.SaveLoadedEventArgs e) => LoadDict();
 
         private void LoadDict()
         {
-            dataDict = Game1.content.Load<Dictionary<int, BackPackData>>(dictPath);
-            foreach (var key in dataDict.Keys.ToArray())
+            try
             {
-                dataDict[key].texture = Helper.GameContent.Load<Texture2D>(dataDict[key].texturePath);
+                dataDict = Game1.content.Load<Dictionary<int, BackPackData>>(dictPath);
+                if (dataDict == null) return;
+                foreach (var key in dataDict.Keys.ToArray())
+                    dataDict[key].texture = Helper.GameContent.Load<Texture2D>(dataDict[key].texturePath);
+                SHelper.GameContent.InvalidateCache("String/UI");
             }
-            SHelper.GameContent.InvalidateCache("String/UI");
+            catch { }
         }
 
         private void Content_AssetRequested(object sender, StardewModdingAPI.Events.AssetRequestedEventArgs e)
         {
             if (e.NameWithoutLocale.IsEquivalentTo(dictPath))
-            {
                 e.LoadFrom(() => new Dictionary<int, BackPackData>(), StardewModdingAPI.Events.AssetLoadPriority.Exclusive);
-            }
             else if (e.NameWithoutLocale.IsEquivalentTo("String/UI"))
-            {
                 e.Edit(EditStrings);
-            }
         }
 
         private void EditStrings(IAssetData obj)
         {
             var editor = obj.AsDictionary<string, string>();
             foreach (var key in dataDict.Keys.ToArray())
-            {
                 editor.Data[$"Chat_CustomBackpack_{key}"] = string.Format(SHelper.Translation.Get("farmer-bought-x"));
-            }
         }
 
         private void GameLoop_GameLaunched(object sender, StardewModdingAPI.Events.GameLaunchedEventArgs e)
         {
             var configMenu = this.Helper.ModRegistry.GetApi<IGenericModConfigMenuApi>("spacechase0.GenericModConfigMenu");
-            if (configMenu is null)
-                return;
+            if (configMenu is null) return;
 
-            configMenu.Register(
-                mod: ModManifest,
-                reset: () => Config = new ModConfig(),
-                save: () => Helper.WriteConfig(Config)
-            );
-
-            configMenu.AddBoolOption(
-                mod: ModManifest,
-                name: () => ModEntry.SHelper.Translation.Get("GMCM_Option_ModEnabled_Name"),
-                getValue: () => Config.ModEnabled,
-                setValue: value => Config.ModEnabled = value
-            );
-            configMenu.AddBoolOption(
-                mod: ModManifest,
-                name: () => ModEntry.SHelper.Translation.Get("GMCM_Option_ShowArrows_Name"),
-                getValue: () => Config.ShowArrows,
-                setValue: value => Config.ShowArrows = value
-            );
-            configMenu.AddBoolOption(
-                mod: ModManifest,
-                name: () => ModEntry.SHelper.Translation.Get("GMCM_Option_ShowRowNumbers_Name"),
-                getValue: () => Config.ShowRowNumbers,
-                setValue: value => Config.ShowRowNumbers = value
-            );
-            configMenu.AddNumberOption(
-                mod: ModManifest,
-                name: () => ModEntry.SHelper.Translation.Get("GMCM_Option_MinHandleHeight_Name"),
-                getValue: () => Config.MinHandleHeight,
-                setValue: value => Config.MinHandleHeight = value
-            );
-            configMenu.AddKeybind(
-                mod: ModManifest,
-                name: () => ModEntry.SHelper.Translation.Get("GMCM_Option_ShowExpandedButton_Name"),
-                getValue: () => Config.ShowExpandedButton,
-                setValue: value => Config.ShowExpandedButton = value
-            );
-            configMenu.AddNumberOption(
-                mod: ModManifest,
-                name: () => ModEntry.SHelper.Translation.Get("GMCM_Option_ShiftRows_Name"),
-                getValue: () => Config.ShiftRows,
-                setValue: value => Config.ShiftRows = value
-            );
+            configMenu.Register(mod: ModManifest, reset: () => Config = new ModConfig(), save: () => Helper.WriteConfig(Config));
+            configMenu.AddBoolOption(mod: ModManifest, name: () => ModEntry.SHelper.Translation.Get("GMCM_Option_ModEnabled_Name"), getValue: () => Config.ModEnabled, setValue: value => Config.ModEnabled = value);
+            configMenu.AddBoolOption(mod: ModManifest, name: () => ModEntry.SHelper.Translation.Get("GMCM_Option_ShowArrows_Name"), getValue: () => Config.ShowArrows, setValue: value => Config.ShowArrows = value);
+            configMenu.AddBoolOption(mod: ModManifest, name: () => ModEntry.SHelper.Translation.Get("GMCM_Option_ShowRowNumbers_Name"), getValue: () => Config.ShowRowNumbers, setValue: value => Config.ShowRowNumbers = value);
+            configMenu.AddNumberOption(mod: ModManifest, name: () => ModEntry.SHelper.Translation.Get("GMCM_Option_MinHandleHeight_Name"), getValue: () => Config.MinHandleHeight, setValue: value => Config.MinHandleHeight = value);
+            configMenu.AddKeybind(mod: ModManifest, name: () => ModEntry.SHelper.Translation.Get("GMCM_Option_ShowExpandedButton_Name"), getValue: () => Config.ShowExpandedButton, setValue: value => Config.ShowExpandedButton = value);
+            configMenu.AddNumberOption(mod: ModManifest, name: () => ModEntry.SHelper.Translation.Get("GMCM_Option_ShiftRows_Name"), getValue: () => Config.ShiftRows, setValue: value => Config.ShiftRows = value);
         }
     }
 }
