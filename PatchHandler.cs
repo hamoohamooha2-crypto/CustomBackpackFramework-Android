2afa4037ededdf1b71f10e0376ddbc7e527da7d1
hamoohamooha2-crypto <hamoohamooha2@gmail.com>
2025-12-24 12:33:48 +0700

    Update PatchHandler.cs

--- PatchHandler.cs
+++ PatchHandler.cs
@@ -6,6 +6,7 @@ using StardewValley.Menus;
 using System;
 using System.Collections.Generic;
 using System.Reflection;
+using StardewModdingAPI;
 
 namespace CustomBackpack
 {
@@ -13,14 +14,18 @@ namespace CustomBackpack
     {
         public static void SafePatchAll(Harmony harmony, Type patchClass)
         {
-            var invConstructor = AccessTools.Constructor(typeof(InventoryMenu), new[] { typeof(int), typeof(int), typeof(bool), typeof(IList<Item>), typeof(InventoryMenu.highlightThisItem), typeof(int), typeof(int), typeof(int), typeof(int), typeof(bool), typeof(bool) })
-                                ?? AccessTools.Constructor(typeof(InventoryMenu), new[] { typeof(int), typeof(int), typeof(bool), typeof(IList<Item>), typeof(InventoryMenu.highlightThisItem), typeof(int), typeof(int), typeof(int), typeof(int), typeof(bool) });
+            var invConstructor = AccessTools.Constructor(typeof(InventoryMenu), new[] { 
+                typeof(int), typeof(int), typeof(bool), typeof(IList<Item>), 
+                typeof(InventoryMenu.highlightThisItem), typeof(int), typeof(int), 
+                typeof(int), typeof(int), typeof(bool), typeof(bool) 
+            });
             
             if (invConstructor != null)
                 harmony.Patch(invConstructor, postfix: new HarmonyMethod(patchClass, "InventoryMenu_Postfix"));
 
-            var drawMethod = AccessTools.Method(typeof(InventoryMenu), nameof(InventoryMenu.draw), new[] { typeof(SpriteBatch), typeof(int), typeof(int), typeof(int) })
-                            ?? AccessTools.Method(typeof(InventoryMenu), nameof(InventoryMenu.draw), new[] { typeof(SpriteBatch), typeof(int), typeof(int) });
+            var drawMethod = AccessTools.Method(typeof(InventoryMenu), nameof(InventoryMenu.draw), new[] { 
+                typeof(SpriteBatch), typeof(int), typeof(int), typeof(int) 
+            });
 
             if (drawMethod != null)
             {
@@ -28,24 +33,42 @@ namespace CustomBackpack
                 harmony.Patch(drawMethod, postfix: new HarmonyMethod(patchClass, "InventoryMenu_draw_Postfix"));
             }
 
-            var rightClick = AccessTools.Method(typeof(InventoryMenu), nameof(InventoryMenu.rightClick), new[] { typeof(int), typeof(int), typeof(Item), typeof(bool), typeof(bool) })
-                            ?? AccessTools.Method(typeof(InventoryMenu), nameof(InventoryMenu.rightClick), new[] { typeof(int), typeof(int), typeof(Item) });
+            var rightClick = AccessTools.Method(typeof(InventoryMenu), nameof(InventoryMenu.rightClick), new[] { 
+                typeof(int), typeof(int), typeof(Item), typeof(bool), typeof(bool) 
+            });
 
             if (rightClick != null)
                 harmony.Patch(rightClick, prefix: new HarmonyMethod(patchClass, "InventoryMenu_rightClick_Prefix"));
 
-            var performAction = AccessTools.Method(typeof(GameLocation), nameof(GameLocation.performAction), new[] { typeof(string[]), typeof(Farmer), typeof(xTile.Dimensions.Location) })
-                               ?? AccessTools.Method(typeof(GameLocation), nameof(GameLocation.performAction), new[] { typeof(string[]), typeof(Farmer), typeof(Location) });
+            var performAction = AccessTools.Method(typeof(GameLocation), nameof(GameLocation.performAction), new[] { 
+                typeof(string[]), typeof(Farmer), typeof(xTile.Dimensions.Location) 
+            });
 
             if (performAction != null)
                 harmony.Patch(performAction, prefix: new HarmonyMethod(patchClass, "GameLocation_performAction_Prefix"));
 
-            var answerDialogue = AccessTools.Method(typeof(GameLocation), nameof(GameLocation.answerDialogueAction), new[] { typeof(string), typeof(string[]) });
+            var answerDialogue = AccessTools.Method(typeof(GameLocation), nameof(GameLocation.answerDialogueAction), new[] { 
+                typeof(string), typeof(string[]) 
+            });
+            
             if (answerDialogue != null)
                 harmony.Patch(answerDialogue, prefix: new HarmonyMethod(patchClass, "GameLocation_answerDialogueAction_Prefix"));
 
-            harmony.Patch(AccessTools.Method(typeof(Farmer), nameof(Farmer.shiftToolbar)), prefix: new HarmonyMethod(patchClass, "Farmer_shiftToolbar_Prefix"));
-            harmony.Patch(AccessTools.Method(typeof(IClickableMenu), nameof(IClickableMenu.applyMovementKey)), prefix: new HarmonyMethod(patchClass, "IClickableMenu_applyMovementKey_Prefix"));
+            var shiftToolbar = AccessTools.Method(typeof(Farmer), nameof(Farmer.shiftToolbar));
+            if (shiftToolbar != null)
+                harmony.Patch(shiftToolbar, prefix: new HarmonyMethod(patchClass, "Farmer_shiftToolbar_Prefix"));
+
+            var applyMovement = AccessTools.Method(typeof(IClickableMenu), nameof(IClickableMenu.applyMovementKey));
+            if (applyMovement != null)
+                harmony.Patch(applyMovement, prefix: new HarmonyMethod(patchClass, "IClickableMenu_applyMovementKey_Prefix"));
+
+            var shopHover = AccessTools.Method(typeof(ShopMenu), nameof(ShopMenu.performHoverAction));
+            if (shopHover != null)
+                harmony.Patch(shopHover, prefix: new HarmonyMethod(patchClass, "ShopMenu_performHoverAction_Prefix"));
+            
+            var invHover = AccessTools.Method(typeof(InventoryMenu), nameof(InventoryMenu.hover));
+            if (invHover != null)
+                harmony.Patch(invHover, prefix: new HarmonyMethod(patchClass, "InventoryMenu_hover_Prefix"));
         }
     }
 }
