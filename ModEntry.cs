f0439730e9fe278a073677164ef84b75b2d90cd7
hamoohamooha2-crypto <hamoohamooha2@gmail.com>
2025-12-24 11:28:54 +0700

    Update ModEntry.cs

--- ModEntry.cs
+++ ModEntry.cs
@@ -24,6 +24,7 @@ namespace CustomBackpack
         public static ModEntry context;
 
         public static string dictPath = "platinummyr.CustomBackpackFramework/dictionary";
+
         public static Dictionary<int, BackPackData> dataDict = new Dictionary<int, BackPackData>();
         public static int IDOffset = 0;
         public static Texture2D scrollTexture;
@@ -31,6 +32,7 @@ namespace CustomBackpack
 
         public static PerScreen<IClickableMenu> lastMenu = new PerScreen<IClickableMenu>();
         public static PerScreen<int> pressTime = new PerScreen<int>();
+
         public static PerScreen<int> oldScrollValue = new PerScreen<int>();
         public static PerScreen<int> oldCapacity = new PerScreen<int>();
         public static PerScreen<int> oldRows = new PerScreen<int>();
@@ -44,7 +46,9 @@ namespace CustomBackpack
         public override void Entry(IModHelper helper)
         {
             Config = Helper.ReadConfig<ModConfig>();
-            if (!Config.ModEnabled) return;
+
+            if (!Config.ModEnabled)
+                return;
 
             context = this;
             SMonitor = Monitor;
@@ -57,10 +61,11 @@ namespace CustomBackpack
             helper.Events.Input.ButtonPressed += Input_ButtonPressed;
             helper.Events.GameLoop.UpdateTicked += GameLoop_UpdateTicked;
 
-            helper.ConsoleCommands.Add("custombackpack", "Usage: custombackpack <slotnumber>", SetSlots);
+            helper.ConsoleCommands.Add("custombackpack", "Manually set backpack slots. Usage: custombackpack <slotnumber>", SetSlots);
 
             var harmony = new Harmony(ModManifest.UniqueID);
 
+            // 1.6 Constructor Patch (11 parameters)
             harmony.Patch(
                 original: AccessTools.Constructor(typeof(InventoryMenu), new Type[] { typeof(int), typeof(int), typeof(bool), typeof(IList<Item>), typeof(InventoryMenu.highlightThisItem), typeof(int), typeof(int), typeof(int), typeof(int), typeof(bool), typeof(bool) }),
                 postfix: new HarmonyMethod(typeof(ObjectPatches), nameof(ObjectPatches.InventoryMenu_Postfix))
@@ -144,162 +149,6 @@ namespace CustomBackpack
 
             harmony.Patch(
                 original: AccessTools.Method(typeof(Farmer), nameof(Farmer.shiftToolbar), new Type[] { typeof(bool) }),
-                prefix: new HarmonyMethod(typeof(ObjectPatches), nameof(ObjectPatches.Farmer_shiftToolbar_Prefix))
-            );
-
-            scrollTexture = new Texture2D(Game1.graphics.GraphicsDevice, 1, 1);
-            scrollTexture.SetData(new Color[] { Config.BackgroundColor });
-            handleTexture = new Texture2D(Game1.graphics.GraphicsDevice, 1, 1);
-            handleTexture.SetData(new Color[] { Config.HandleColor });
-        }
-
-        private void GameLoop_UpdateTicked(object sender, StardewModdingAPI.Events.UpdateTickedEventArgs e)
-        {
-            if (scrolling.Value && (Game1.activeClickableMenu is null || Game1.input.GetMouseState().LeftButton != ButtonState.Pressed))
-                scrolling.Value = false;
-            
-            var newScrollValue = Game1.input.GetMouseState().ScrollWheelValue;
-            scrollChange.Value = (oldScrollValue.Value > newScrollValue) ? 1 : (oldScrollValue.Value < newScrollValue ? -1 : 0);
-            oldScrollValue.Value = newScrollValue;
-        }
-
-        private void Input_ButtonPressed(object sender, StardewModdingAPI.Events.ButtonPressedEventArgs e)
-        {
-            if (Game1.activeClickableMenu is not null && e.Button == SButton.MouseLeft && scrollArea.Value.Contains(Game1.getMouseX(), Game1.getMouseY()))
-                scrolling.Value = true;
-        }
-
-        public override object GetApi() => new CustomBackpackApi();
-
-        private void SetSlots(string arg1, string[] arg2)
-        {
-            if (arg2.Length == 1 && int.TryParse(arg2[0], out int slots))
-                SetPlayerSlots(slots);
-        }
-
-        private void GameLoop_DayStarted(object sender, StardewModdingAPI.Events.DayStartedEventArgs e) => LoadDict();
-        private void GameLoop_SaveLoaded(object sender, StardewModdingAPI.Events.SaveLoadedEventArgs e) => LoadDict();
-
-        private void LoadDict()
-        {
-            dataDict = Game1.content.Load<Dictionary<int, BackPackData>>(dictPath);
-            foreach (var key in dataDict.Keys.ToArray())
-                dataDict[key].texture = Helper.GameContent.Load<Texture2D>(dataDict[key].texturePath);
-            SHelper.GameContent.InvalidateCache("String/UI");
-        }
-
-        private void Content_AssetRequested(object sender, StardewModdingAPI.Events.AssetRequestedEventArgs e)
-        {
-            if (e.NameWithoutLocale.IsEquivalentTo(dictPath))
-                e.LoadFrom(() => new Dictionary<int, BackPackData>(), StardewModdingAPI.Events.AssetLoadPriority.Exclusive);
-            else if (e.NameWithoutLocale.IsEquivalentTo("String/UI"))
-                e.Edit(EditStrings);
-        }
-
-        private void EditStrings(IAssetData obj)
-        {
-            var editor = obj.AsDictionary<string, string>();
-            foreach (var key in dataDict.Keys.ToArray())
-                editor.Data[$"Chat_CustomBackpack_{key}"] = string.Format(SHelper.Translation.Get("farmer-bought-x"));
-        }
-
-        private void GameLoop_GameLaunched(object sender, StardewModdingAPI.Events.GameLaunchedEventArgs e)
-        {
-            var configMenu = this.Helper.ModRegistry.GetApi<IGenericModConfigMenuApi>("spacechase0.GenericModConfigMenu");
-            if (configMenu is null) return;
-
-            configMenu.Register(mod: ModManifest, reset: () => Config = new ModConfig(), save: () => Helper.WriteConfig(Config));
-            configMenu.AddBoolOption(mod: ModManifest, name: () => ModEntry.SHelper.Translation.Get("GMCM_Option_ModEnabled_Name"), getValue: () => Config.ModEnabled, setValue: value => Config.ModEnabled = value);
-            configMenu.AddBoolOption(mod: ModManifest, name: () => ModEntry.SHelper.Translation.Get("GMCM_Option_ShowArrows_Name"), getValue: () => Config.ShowArrows, setValue: value => Config.ShowArrows = value);
-            configMenu.AddBoolOption(mod: ModManifest, name: () => ModEntry.SHelper.Translation.Get("GMCM_Option_ShowRowNumbers_Name"), getValue: () => Config.ShowRowNumbers, setValue: value => Config.ShowRowNumbers = value);
-            configMenu.AddNumberOption(mod: ModManifest, name: () => ModEntry.SHelper.Translation.Get("GMCM_Option_MinHandleHeight_Name"), getValue: () => Config.MinHandleHeight, setValue: value => Config.MinHandleHeight = value);
-            configMenu.AddKeybind(mod: ModManifest, name: () => ModEntry.SHelper.Translation.Get("GMCM_Option_ShowExpandedButton_Name"), getValue: () => Config.ShowExpandedButton, setValue: value => Config.ShowExpandedButton = value);
-            configMenu.AddNumberOption(mod: ModManifest, name: () => ModEntry.SHelper.Translation.Get("GMCM_Option_ShiftRows_Name"), getValue: () => Config.ShiftRows, setValue: value => Config.ShiftRows = value);
-        }
-    }
-}            harmony.Patch(
-                original: AccessTools.Constructor(typeof(InventoryMenu), new Type[] { typeof(int), typeof(int), typeof(bool), typeof(IList<Item>), typeof(InventoryMenu.highlightThisItem), typeof(int), typeof(int), typeof(int), typeof(int), typeof(bool), typeof(bool) }),
-                postfix: new HarmonyMethod(typeof(ObjectPatches), nameof(ObjectPatches.InventoryMenu_Postfix))
-            );
-
-            harmony.Patch(
-                original: AccessTools.Method(typeof(InventoryMenu), nameof(InventoryMenu.hover)),
-                prefix: new HarmonyMethod(typeof(ObjectPatches), nameof(ObjectPatches.InventoryMenu_hover_Prefix))
-            );
-
-            harmony.Patch(
-                original: AccessTools.Method(typeof(ShopMenu), nameof(ShopMenu.performHoverAction)),
-                prefix: new HarmonyMethod(typeof(ObjectPatches), nameof(ObjectPatches.ShopMenu_performHoverAction_Prefix))
-            );
-
-            harmony.Patch(
-                original: AccessTools.Method(typeof(ShopMenu), nameof(ShopMenu.receiveScrollWheelAction)),
-                prefix: new HarmonyMethod(typeof(ObjectPatches), nameof(ObjectPatches.ShopMenu_receiveScrollWheelAction_Prefix))
-            );
-
-            harmony.Patch(
-                original: AccessTools.Method(typeof(InventoryMenu), nameof(InventoryMenu.getInventoryPositionOfClick)),
-                prefix: new HarmonyMethod(typeof(ObjectPatches), nameof(ObjectPatches.InventoryMenu_getInventoryPositionOfClick_Prefix))
-            );
-
-            harmony.Patch(
-                original: AccessTools.Method(typeof(InventoryMenu), nameof(InventoryMenu.leftClick)),
-                prefix: new HarmonyMethod(typeof(ObjectPatches), nameof(ObjectPatches.InventoryMenu_leftClick_Prefix))
-            );
-
-            harmony.Patch(
-                original: AccessTools.Method(typeof(InventoryMenu), nameof(InventoryMenu.rightClick)),
-                prefix: new HarmonyMethod(typeof(ObjectPatches), nameof(ObjectPatches.InventoryMenu_rightClick_Prefix))
-            );
-
-            harmony.Patch(
-                original: AccessTools.Method(typeof(InventoryMenu), nameof(InventoryMenu.setUpForGamePadMode)),
-                postfix: new HarmonyMethod(typeof(ObjectPatches), nameof(ObjectPatches.InventoryMenu_setUpForGamePadMode_Postfix))
-            );
-
-            harmony.Patch(
-                original: AccessTools.Method(typeof(IClickableMenu), nameof(IClickableMenu.applyMovementKey), new Type[] { typeof(int) }),
-                prefix: new HarmonyMethod(typeof(ObjectPatches), nameof(ObjectPatches.IClickableMenu_applyMovementKey_Prefix))
-            );
-
-            harmony.Patch(
-                original: AccessTools.Method(typeof(ItemGrabMenu), "customSnapBehavior"),
-                prefix: new HarmonyMethod(typeof(ObjectPatches), nameof(ObjectPatches.ItemGrabMenu_customSnapBehavior_Prefix))
-            );
-
-            harmony.Patch(
-                original: AccessTools.Method(typeof(InventoryMenu), nameof(InventoryMenu.draw), new Type[] { typeof(SpriteBatch), typeof(int), typeof(int), typeof(int) }),
-                prefix: new HarmonyMethod(typeof(ObjectPatches), nameof(ObjectPatches.InventoryMenu_draw_Prefix)),
-                postfix: new HarmonyMethod(typeof(ObjectPatches), nameof(ObjectPatches.InventoryMenu_draw_Postfix))
-            );
-
-            harmony.Patch(
-                original: AccessTools.Method(typeof(SeedShop), nameof(SeedShop.draw)),
-                prefix: new HarmonyMethod(typeof(ObjectPatches), nameof(ObjectPatches.SeedShop_draw_Prefix))
-            );
-
-            harmony.Patch(
-                original: AccessTools.Method(typeof(GameLocation), nameof(GameLocation.performAction), new Type[] { typeof(string[]), typeof(Farmer), typeof(Location) }),
-                prefix: new HarmonyMethod(typeof(ObjectPatches), nameof(ObjectPatches.GameLocation_performAction_Prefix))
-            );
-
-            harmony.Patch(
-                original: AccessTools.Method(typeof(GameLocation), nameof(GameLocation.answerDialogueAction), new Type[] { typeof(string), typeof(string[]) }),
-                prefix: new HarmonyMethod(typeof(ObjectPatches), nameof(ObjectPatches.GameLocation_answerDialogueAction_Prefix))
-            );
-
-            harmony.Patch(
-                original: AccessTools.PropertyGetter(typeof(SpecialItem), "displayName"),
-                prefix: new HarmonyMethod(typeof(ObjectPatches), nameof(ObjectPatches.SpecialItem_displayName_Prefix))
-            );
-
-            harmony.Patch(
-                original: AccessTools.Method(typeof(SpecialItem), nameof(SpecialItem.getTemporarySpriteForHoldingUp)),
-                prefix: new HarmonyMethod(typeof(ObjectPatches), nameof(ObjectPatches.SpecialItem_getTemporarySpriteForHoldingUp_Prefix))
-            );
-
-            harmony.Patch(
-                original: AccessTools.Method(typeof(Farmer), nameof(Farmer.shiftToolbar)),
                 prefix: new HarmonyMethod(typeof(ObjectPatches), nameof(ObjectPatches.Farmer_shiftToolbar_Prefix))
             );
 
